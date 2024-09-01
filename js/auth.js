25fe1b82bf6d642732eaec32cc7bbe7f2b2efa96
Md. Jubaer Mahmud Sarker <jmsman3@gmail.com>
2024-09-01 10:12:39 +0600

    registration altert solve

--- js/auth.js
+++ js/auth.js
@@ -37,35 +37,76 @@ const updateNavBar = () => {
 window.onload = updateNavBar;
 
 //Registaration korar function
+// const handleRegistration = (event) => {
+//    event.preventDefault();
+//    const username = getValue("username");
+//    const first_name = getValue("first_name");
+//    const last_name = getValue("last_name");
+//    const email = getValue("email");
+//    const password = getValue("password");
+//    const confirm_password = getValue("confirm_password");
+
+//    const info = { username, first_name, last_name, email, password, confirm_password };
+
+//    if (password === confirm_password) {
+//        document.getElementById("error").innerText = "";
+//        if (/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)) {
+//            fetch("https://food-project-9vo4.onrender.com/user/register/", {
+//                method: "POST",
+//                headers: { "content-type": "application/json" },
+//                body: JSON.stringify(info)
+//            })
+//            .then(res => res.json())
+//            .then(data => console.log(data));
+//        } else {
+//            document.getElementById("error").innerText = "Password must include minimum eight characters, at least one uppercase letter, one lowercase letter, and one number";
+//        }
+//    } else {
+//        document.getElementById("error").innerText = "Password and confirm password do not match";
+//        alert("Password and confirm password do not match");
+//    }
+// };
+
 const handleRegistration = (event) => {
-   event.preventDefault();
-   const username = getValue("username");
-   const first_name = getValue("first_name");
-   const last_name = getValue("last_name");
-   const email = getValue("email");
-   const password = getValue("password");
-   const confirm_password = getValue("confirm_password");
+    event.preventDefault();
+    const username = getValue("username");
+    const first_name = getValue("first_name");
+    const last_name = getValue("last_name");
+    const email = getValue("email");
+    const password = getValue("password");
+    const confirm_password = getValue("confirm_password");
 
-   const info = { username, first_name, last_name, email, password, confirm_password };
+    const info = { username, first_name, last_name, email, password, confirm_password };
 
-   if (password === confirm_password) {
-       document.getElementById("error").innerText = "";
-       if (/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)) {
-           fetch("https://food-project-9vo4.onrender.com/user/register/", {
-               method: "POST",
-               headers: { "content-type": "application/json" },
-               body: JSON.stringify(info)
-           })
-           .then(res => res.json())
-           .then(data => console.log(data));
-       } else {
-           document.getElementById("error").innerText = "Password must include minimum eight characters, at least one uppercase letter, one lowercase letter, and one number";
-       }
-   } else {
-       document.getElementById("error").innerText = "Password and confirm password do not match";
-       alert("Password and confirm password do not match");
-   }
+    if (password === confirm_password) {
+        document.getElementById("error").innerText = "";
+        if (/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(password)) {
+            fetch("https://food-project-9vo4.onrender.com/user/register/", {
+                method: "POST",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify(info)
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (!data.errors) {
+                    // Show alert message
+                    alert("Check your email for confirmation");
+                    // Redirect to login.html after successful registration
+                    window.location.href = "login.html";
+                } else {
+                    document.getElementById("error").innerText = data.errors;
+                }
+            });
+        } else {
+            document.getElementById("error").innerText = "Password must include minimum eight characters, at least one uppercase letter, one lowercase letter, and one number";
+        }
+    } else {
+        document.getElementById("error").innerText = "Password and confirm password do not match";
+        alert("Password and confirm password do not match");
+    }
 };
+
 
 // Get value from input by ID
 const getValue = (id) => {
